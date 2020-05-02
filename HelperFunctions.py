7dd1c44041e76125d7291c07c3572386e5778692
hh2110 <hash@tessella.co.uk>
2020-05-02 13:30:41 +0100

    creating functions

---
+++ HelperFunctions.py
@@ -0,0 +1,115 @@
+import os
+import configparser
+from datetime import datetime, timedelta
+import logging
+from twilio.rest import Client
+
+
+config = configparser.ConfigParser()
+config.read("shops.conf")
+
+logging.basicConfig(
+    filename="asda.log",
+    filemode="a",
+    format="%(asctime)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+def SetUpEnvironmentVariables(groceryStore):
+    """
+    sets up environment with information based on the grocery-store
+    param: groceryStore str
+    return: none
+    """
+    parameterList = [
+        "POSTCODE",
+        "LATITUDE",
+        "LONGITUDE",
+        "ACT_ID",
+        "TWILIO_ACT_SID",
+        "TWILIO_AUTH_TOKEN",
+        "TWILIO_NUMBER",
+        "MY_NUMBER",
+    ]
+
+    if os.environ["COMPUTERNAME"] == "UK-L-0318":
+        for parameter in parameterList:
+            os.environ[parameter] = config[groceryStore][parameter]
+
+    os.environ["URL"] = config[groceryStore]["URL"]
+    os.environ["START_DATE"] = datetime.today().date().strftime("%Y-%m-%dT%H:%M:%S")
+    os.environ["END_DATE"] = (datetime.today().date() + timedelta(days=14)).strftime(
+        "%Y-%m-%dT%H:%M:%S"
+    )
+
+
+def SetUpJsonFileForRequest(groceryStore):
+    """
+    sets up json string for the request
+    param: groceryStore str
+    return: none
+    """
+    json_data = {
+        "requestorigin": "gi",
+        "data": {
+            "service_info": {"fulfillment_type": "DELIVERY", "enable_express": "false"},
+            "start_date": os.environ["START_DATE"],
+            "end_date": os.environ["END_DATE"],
+            "reserved_slot_id": "",
+            "service_address": {
+                "postcode": os.environ["POSTCODE"],
+                "latitude": os.environ["LATITUDE"],
+                "longitude": os.environ["LONGITUDE"],
+            },
+            "customer_info": {"account_id": os.environ["ACT_ID"]},
+            "order_info": {
+                "order_id": "20983571638",
+                "restricted_item_types": [],
+                "volume": 0,
+                "weight": 0,
+                "sub_total_amount": 0,
+                "line_item_count": 0,
+                "total_quantity": 0,
+            },
+        },
+    }
+
+    return json_data
+
+
+def PopulateSlotData(requestString):
+    """
+    parse the string returned from the url request
+    param: requestString str
+    return: dictionary containing info about slots
+    """
+    # Initialise empty dictionary for data
+    slot_data = {}
+
+    # Loop through json response and record slot status for each time slot
+    if requestString.json()['data']:
+        for slot_day in requestString.json()["data"]["slot_days"]:
+            slot_date = slot_day["slot_date"]
+            for slot in slot_day["slots"]:
+                slot_time = slot["slot_info"]["start_time"]
+                slot_time = datetime.strptime(slot_time, "%Y-%m-%dT%H:%M:%SZ")
+                slot_status = slot["slot_info"]["status"]
+                slot_data[slot_time.strftime("%H:%M:%S %d-%m-%Y")] = slot_status
+    else:
+        logging.info("No slot data to look into.")
+
+    return slot_data
+
+
+def SendTextMessage(messageTxt):
+    """
+    sending message with twilio
+    """
+    account_sid = os.environ["TWILIO_ACT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body=messageTxt, from_=os.environ["TWILIO_NUMBER"], to=os.environ["MY_NUMBER"]
+    )
---
+++ get-grocery-delivery-slots-hh.py
@@ -0,0 +1,38 @@
+import os
+from HelperFunctions import (
+    SetUpEnvironmentVariables, SetUpJsonFileForRequest, PopulateSlotData,
+    SendTextMessage
+)
+import requests
+import logging
+
+
+logging.basicConfig(
+    filename="asda.log",
+    filemode="a",
+    format="%(asctime)s - %(message)s",
+    level=logging.INFO,
+)
+
+store = "ASDA"
+
+SetUpEnvironmentVariables(store)
+json_data = SetUpJsonFileForRequest(store)
+
+# Make POST request to API, sending required json data
+r = requests.post(os.environ["URL"], json=json_data)
+
+slot_data = PopulateSlotData(r)
+
+# Filter for available slots
+available_list = [
+    f"\n{key} - {value}" for (key, value) in slot_data.items() if value != "UNAVAILABLE"
+]
+
+# If any available slots exist, send a text notification
+if len(available_list) > 0:
+    SendTextMessage(available_list[0])
+    logging.info("Message sent inlcuding {}".format(available_list[0]))
+else:
+    logging.info("No available slots")
+
